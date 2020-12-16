4b859068bcf2c49a1834edd57d74bf74088f6c82
Derek Fujimoto <fujimoto@phas.ubc.ca>
2020-12-16 11:21:52 -0800

    v0.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rigur",
-    version="0.0.4",
+    version="0.0.5",
     author="Derek Fujimoto",
     author_email="fujimoto@phas.ubc.ca",
     description="Rip data from images of figures",
